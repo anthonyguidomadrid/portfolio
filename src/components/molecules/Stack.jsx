51e7f7e0f8f04995494f0febba5bf8b2d96d66fa
Anthony Guido <anthony.guido@mifarma.es>
2023-01-03 18:53:58 +0100

    improve design stack section

--- src/components/molecules/Stack.jsx
+++ src/components/molecules/Stack.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 import { removeEmptySpaceAndHighCase } from '../../helpers/tranformString'
 import { useIsFirstRender } from '../../customHooks/useIsFirstRender' 
 import classNames from 'classnames'
+import { ReactComponent as Chevron } from '../../assets/Chevron.svg'
+
 
 export const Stack = ({stackContent}) => {
     const {title, subtitle, technologies} = stackContent
@@ -11,13 +13,14 @@ export const Stack = ({stackContent}) => {
 
     useEffect(() => {
         if (isFirstRender && technologies.length > 0) {
-            return setAccordionOpen(technologies.reduce((o, key) => ({ ...o, [key.title]: false}), {})) 
+            return setAccordionOpen(technologies.reduce((o, key) => ({ ...o, [removeEmptySpaceAndHighCase(key.title)]: false}), {})) 
         }
     }, [isFirstRender, technologies])
 
     const handleAccordionClick = (item) => {
+        const key = removeEmptySpaceAndHighCase(item)
         const obj = {...isAccordionOpen}
-        obj[item] = !obj[item]
+        obj[key] = !obj[key]
         return setAccordionOpen(obj)
     }
 
@@ -27,18 +30,18 @@ export const Stack = ({stackContent}) => {
                 <h2 className='uppercase font-bold text-xl'>{title}</h2>
                 {subtitle && <p className='font-mono font-semibold md:text-2xl mt-7'>{subtitle}</p>}
             </div>}
-            <div className="flex flex-col">
-                {technologies?.map((item, idx) => {
+            <div className="flex flex-col lg:px-24 transition-all duration-500">
+                {isAccordionOpen && technologies?.map((item, idx) => {
                     const { title, techList } = item
+                    const isOpen = isAccordionOpen?.[removeEmptySpaceAndHighCase(title)]
                     return (
-                        <div key={idx}>
-                            <div className='flex justify-between'>
-                                <p>{title}</p>
-                                <button onClick={() => handleAccordionClick(title)}>click</button>
+                        <div key={idx} className={classNames({'border-b' : idx + 1 !== technologies.length })}>
+                            <div className={classNames('flex justify-between p-5 transition-all duration-500', { 'pb-10' : isOpen})}>
+                                <p className='font-extrabold text-lg'>{title}</p>
+                                <button onClick={() => handleAccordionClick(title)}><Chevron className={classNames({'rotate-270' : isOpen})}/></button>
                             </div>
-                            {console.log('isAccordionOpen', isAccordionOpen, 'isAccordionOpen?.[title]', isAccordionOpen?.[title])}
-                            {techList?.length > 0 && <div className={classNames('flex flex-col', {'hidden' : !isAccordionOpen?.[title]})}>{techList.map((item, idx) => {
-                                return <span key={idx}>{item}</span>
+                            {techList?.length > 0 && <div className={classNames('grid grid-flow-col gap-4 overflow-hidden transition-all max-h-0 duration-500 px-5', {'max-h-96 pb-5' : isOpen}, {'sm:grid-rows-3' : techList.length >= 3}, {'grid-rows-5' : techList.length >= 5}, {'grid-rows-2' : techList.length < 5})}>{techList.map((item, idx) => {
+                                return <span className='mb-5' key={idx}>{item}</span>
                             })}</div>}
                         </div>
                     )
