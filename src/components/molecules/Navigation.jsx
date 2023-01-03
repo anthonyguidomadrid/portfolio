5816398596bc3379ba5f18e12a7b7c3061d9a611
Anthony Guido <anthony.guido@mifarma.es>
2023-01-03 22:27:45 +0100

    improve navigation

--- src/components/molecules/Navigation.jsx
+++ src/components/molecules/Navigation.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import { Button } from '../atoms/Button'
 import { removeEmptySpaceAndHighCase } from '../../helpers/tranformString'
 import classNames from 'classnames'
+import { ReactComponent as HamburgerMenu } from '../../assets/HamburgerMenu.svg'
+
 
 export const Navigation = ({logo, menuItems, cta}) => {
     const [isMenuOpen, setMenuOpen] = useState(false)
@@ -18,20 +20,20 @@ export const Navigation = ({logo, menuItems, cta}) => {
     window.onscroll = () => scrollFunction()
 
     return (
-        <nav className={classNames("flex items-center justify-between flex-wrap p-6 fixed top-0 transition ease-in-out delay-150 w-full z-50", {'bg-slate-900 bg-opacity-80': ispageScrolled || isMenuOpen})}>
+        <nav className={classNames("flex items-center justify-between flex-wrap p-6 fixed top-0 w-full z-50", {'bg-slate-900 bg-opacity-80': ispageScrolled || isMenuOpen})}>
             <div className="flex items-center flex-shrink-0 text-white mr-6">
             <a href="/"><img src={logo?.url} alt={logo?.description} className="h-7 w-7"></img></a>
             </div>
             <div className="block md:hidden">
                 <button className="flex items-center px-3 py-2 border text-white border-white-400 hover:text-white hover:border-white" onClick={() => setMenuOpen(!isMenuOpen)}>
-                    <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                    <HamburgerMenu />
                 </button>
             </div>
-            <div className={classNames("w-full block flex-grow md:flex md:items-center md:w-auto ", {hidden: !isMenuOpen})}>
+            <div className={classNames("w-full block flex-grow md:flex md:items-center md:w-auto transition-all duration-500", {'opacity-0 overflow-hidden md:h-fit h-0 md:opacity-100': !isMenuOpen})}>
                 <div className="text-sm md:flex-grow">
                 {menuItems.map((item, idx) => {
                     const id = removeEmptySpaceAndHighCase(item)
-                    return <a key={idx} href={`/#${id}`} className="block mt-4 md:inline-block md:mt-0 text-slate-200 hover:text-white mr-4">{item}</a>
+                    return <a key={idx} href={`/#${id}`} onClick={() => setMenuOpen(false)} className="block mt-4 md:inline-block md:mt-0 text-slate-200 hover:text-white mr-4">{item}</a>
                 })}
                 </div>
                 <div>
